9aec5ed030ae29c68551546295a18a11ce9262df
Saikrishna1409 <95297408+Saikrishna1409@users.noreply.github.com>
2025-12-30 14:56:27 +0530

    Update Number Pyramid.js

--- 06-javascript/exercises/exercise-03/Number Pyramid.js
+++ 06-javascript/exercises/exercise-03/Number Pyramid.js
@@ -1,12 +1,13 @@
-
-function printPyramid(n){
-for(let i=1;i<=n;i++){
-  let ch=""
-  for(let j=1;j<=n-i;j++) ch+=" "
-  for(let j=1;j<=i;j++) ch+=(j);
-  for(let j=i-1;j>0;j--) ch+=j
-  console.log(ch);
-}
-}
+const printPyramid = (n) => {
+    const rows = Array.from({ length: n }, (_, i) => i + 1);
+    
+    rows.forEach((rowNum) => {
+        const spaces = ' '.repeat(n - rowNum);
+        const numbersAsc = Array.from({ length: rowNum }, (_, i) => i + 1).join('');
+        const numbersDesc = Array.from({ length: rowNum - 1 }, (_, i) => rowNum - 1 - i).join('');
+        
+        console.log(`${spaces}${numbersAsc}${numbersDesc}`);
+    });
+};
 
 printPyramid(5);
