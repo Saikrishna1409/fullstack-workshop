090f3d8c9e6d2ed5498dd9b4437a3c078399e64d
Saikrishna1409 <nyskrishna004@gmail.com>
2026-01-02 09:42:42 +0530

    sql challenges

--- 09-mysql/challenges/top-earners.sql
+++ 09-mysql/challenges/top-earners.sql
@@ -1,7 +1,7 @@
 SELECT department, name, salary, rank_in_dept
 FROM (
     SELECT department, name, salary,
-           ROW_NUMBER() OVER (PARTITION BY department ORDER BY salary DESC) AS rank_in_dept
+        ROW_NUMBER() OVER (PARTITION BY department ORDER BY salary DESC) AS rank_in_dept
     FROM employees
 ) ranked
 WHERE rank_in_dept <= 3
